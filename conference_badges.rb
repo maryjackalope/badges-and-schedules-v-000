59dddc51ad150522315d9a2b0ce8dd82519f390e
Mary Jack <maryjackinthebox@gmail.com>
2018-04-05 01:45:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 
 def badge_maker(name)
-  "Bonjour, je m'appelle #{name}."
+  "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(array)
   speakers.collect do |name|
-  "Bonjour, je m'appelle #{name}."
+  "Hello, my name is #{name}."
   end #do
 end
 
